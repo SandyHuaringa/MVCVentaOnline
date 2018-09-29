b769330b34d65e3465b798a5ec397d1ee4b21aa5
SandyHuaringa <43080680+SandyHuaringa@users.noreply.github.com>
2018-09-28 19:10:22 -0500

    Add files via upload

---
+++ VASQUEZ/modelo/conexion.php
@@ -0,0 +1,8 @@
+<?php
+	$server="localhost";
+	$username="admin";
+	$password="pB69VLR0nkZUOCca";
+	$db='bd_vasquez';
+	$con=mysqli_connect($server,$username,$password,$db)or die("no se ha podido establecer la conexion");
+	
+?>
